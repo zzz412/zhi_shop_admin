58c3b80e1e34fcd54bc3dd3541ad1dc44c09525e
某渣粗 <mouzhacu@163.com>
2022-05-27 17:08:36 +0800

    SKU管理 SKU删除、上架、下架

--- src/api/goods/sku.js
+++ src/api/goods/sku.js
@@ -2,3 +2,15 @@ import request from '@/utils/request'
 
 // 获取SKU列表
 export const reqSkuList = (page, limit) => request({ url: `/admin/product/list/${page}/${limit}`, method: 'get' })
+
+// 上架SKU
+export const reqSaleSku = skuId => request({ url: `/admin/product/onSale/${skuId}`, method: 'get' })
+
+// 下架SKU
+export const reqCancelSaleSku = skuId => request({ url: `/admin/product/cancelSale/${skuId}`, method: 'get' })
+
+// 删除SKU
+export const reqDeleteSku = skuId => request({ url: `/admin/product/deleteSku/${skuId}`, method: 'delete' })
+
+// 获取SKU详情
+export const reqSkuInfo = skuId => request({ url: `/admin/product/getSkuById/${skuId}`, method: 'get' })
--- src/views/goods/sku/index.vue
+++ src/views/goods/sku/index.vue
@@ -19,11 +19,21 @@
         </template>
       </el-table-column>
       <el-table-column label="操作">
-        <template>
+        <template v-slot="{ row }">
+          <!-- 下架 -->
+          <el-tooltip v-if="row.isSale" content="下架商品" placement="top">
+            <el-button size="mini" type="warning" icon="el-icon-bottom" @click="downSaleSku(row.id)" />
+          </el-tooltip>
+          <!-- 上架 -->
+          <el-tooltip v-else content="上架商品" placement="top">
+            <el-button size="mini" type="success" icon="el-icon-top" @click="upSaleSku(row.id)" />
+          </el-tooltip>
+          <!-- 修改 -->
           <el-button size="mini" type="primary" icon="el-icon-edit" />
-          <el-button size="mini" type="primary" icon="el-icon-edit" />
-          <el-button size="mini" type="primary" icon="el-icon-edit" />
-          <el-button size="mini" type="primary" icon="el-icon-edit" />
+          <!-- 详情 -->
+          <el-button size="mini" type="info" icon="el-icon-view" @click="getSkuInfo(row.id)" />
+          <!-- 删除 -->
+          <el-button size="mini" type="danger" icon="el-icon-delete" @click="deleteSku(row.id)" />
         </template>
       </el-table-column>
     </el-table>
@@ -39,11 +49,48 @@
       @current-change="pageChange"
       @size-change="sizeChange"
     />
+    <!-- 抽屉效果 -->
+    <el-drawer
+      size="40%"
+      title="SKU详情"
+      :visible.sync="isShow"
+    >
+      <div class="card">
+        <el-form label-width="80px">
+          <el-form-item label="名称">
+            {{ skuInfo.skuName }}
+          </el-form-item>
+          <el-form-item label="描述">
+            {{ skuInfo.skuDesc }}
+          </el-form-item>
+          <el-form-item label="价格">
+            {{ skuInfo.price }}
+          </el-form-item>
+          <el-form-item label="品牌属性">
+            <el-tag v-for="item in skuInfo.skuAttrValueList" :key="item.id" style="margin-right: 5px;" type="success">
+              {{ item.attrName + '-' + item.valueName }}
+            </el-tag>
+          </el-form-item>
+          <el-form-item label="商品图片">
+            <!-- 走马灯组件 -->
+            <el-carousel height="200">
+              <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+                <el-image
+                  style="width: 100%; height: 100%"
+                  :src="item.imgUrl"
+                  fit="cover"
+                />
+              </el-carousel-item>
+            </el-carousel>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-drawer>
   </el-card>
 </template>
 
 <script>
-import { reqSkuList } from '@/api/goods/sku'
+import { reqSkuList, reqSaleSku, reqCancelSaleSku, reqDeleteSku, reqSkuInfo } from '@/api/goods/sku'
 
 export default {
   name: 'Sku',
@@ -57,7 +104,9 @@ export default {
       // 每页条数
       limit: 5,
       // 总条数
-      total: 0
+      total: 0,
+      isShow: false,
+      skuInfo: {}
     }
   },
   mounted() {
@@ -93,6 +142,30 @@ export default {
       this.page = 1
       // 2. 重新获取品牌列表
       this.getSkuList()
+    },
+    // 上架商品
+    async upSaleSku(id) {
+      await reqSaleSku(id)
+      this.$message.success('操作成功')
+      this.getSkuList()
+    },
+    // 下架商品
+    async downSaleSku(id) {
+      await reqCancelSaleSku(id)
+      this.$message.success('操作成功')
+      this.getSkuList()
+    },
+    // 删除商品
+    async deleteSku(id) {
+      await reqDeleteSku(id)
+      this.$message.success('操作成功')
+      this.getSkuList()
+    },
+    // 获取sku详情
+    async getSkuInfo(id) {
+      const res = await reqSkuInfo(id)
+      this.skuInfo = res.data
+      this.isShow = true
     }
   }
 }
